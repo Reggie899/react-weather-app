86f52efb7d62d8fafc55807aa89654934ff50cc7
Reggie899 <regina.schmidt1212@gmail.com>
2022-05-05 19:56:28 +0200

    api integration in pogress

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,27 +1,55 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
+
 
 import "./WeatherApp.css";
 import SearchSection from "./SearchSection";
 import InfoOutput from "./InfoOutput";
 
 export default function WeatherApp() {
+  const [weatherData, setWeatherData] = useState({ready: false});
+
+  function handleResponse(response) {
+    console.log(response.data);
+  setWeatherData({
+    ready: true, 
+    temperature: response.data.main.temp, 
+    wind: response.data.wind.speed, 
+    city: response.data.name, 
+humidity: response.data.main.humidity,
+todayMin: response.data.main.temp_min, 
+todayMax: response.data.main.temp_max, 
+description:  response.data.weather[0].main,
+icon: response.data.weather[0].icon,
+time: "13:20",
+  });}
+
+if (weatherData.ready) {
   return (
     <div className="WeatherApp">
       <div className="weather">
         <SearchSection />
         <InfoOutput 
-          yourLocation={"Your location"}
-          currentTime={"11:05 AM"}
-          currentDescription={"sunny"}
-          todayTemp={21}
-          todayWeatherPic={"http://openweathermap.org/img/wn/10d@2x.png"}
-          todayMin={15}
-          todayMax={22}
-          humidity={5}
-          wind={1}
+          yourLocation={weatherData.city}
+          currentTime={weatherData.time}
+          currentDescription={weatherData.description}
+          todayTemp={Math.round(weatherData.temperature)}
+          todayWeatherPic={`http://openweathermap.org/img/wn/${weatherData.icon}@2x.png`}
+          todayMin={Math.round(weatherData.todayMin)}
+          todayMax={Math.round(weatherData.todayMax)}
+          humidity={weatherData.humidity}
+          wind={Math.round(weatherData.wind)}
         />
      
       </div>
     </div>
   );
+ } else {
+  let city = "Miami";
+  const apiKey = "e49d8a2ceb4c7b4bb750c995e9734044";
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+ 
+ return "loading ..."
+ }
 }
